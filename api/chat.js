6664b683ffa2ae4ef885823e1e7aa82fe25defc9
Emma Murphy <127850221+murphe80@users.noreply.github.com>
2026-03-15 13:10:04 +0000

    local development URL

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const ALLOWED_ORIGINS = ["https://docs.formulatrinity.ie", "127.0.0.1:8000"];
+const ALLOWED_ORIGINS = ["https://docs.formulatrinity.ie", "http://127.0.0.1:8000/"];
 
 export default async function handler(req, res) {
   const origin = req.headers.origin || "";
